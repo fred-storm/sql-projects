c038a22d16033bb4bf7a4a359b011a5f58d08da5
fred-storm <fredstorm@icloud.com>
2026-01-29 22:30:06 -0600

    Starting the music database script.

---
+++ music-db.sql
@@ -0,0 +1,17 @@
+/* Simple database to house some simple tables to experiment with SQL and eventually hooking 
+python into.
+
+Author: Adam Frederick
+Date: 29 Jan 2026
+*/
+
+-- Create the DB
+Create DATABASE musicdb;
+GO
+
+-- Switch to new DB
+USE musicdb;
+GO
+
+/* Now I will create all the things I need in the 
+database to suit my needs */
